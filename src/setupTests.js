3c70324c372184f52f27b259f0e3633d61a79db5
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-14 12:06:08 +0100

    refactor: extract enzyme config to setupTests.js file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,8 @@
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
 const mockGeolocation = {
   getCurrentPosition: jest.fn(),
   watchPosition: jest.fn()
